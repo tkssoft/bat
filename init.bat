6065f31cdd9020605d564b9109ea77030d4ed127
unknown <shulkevich@gmail.com>
2020-12-30 15:10:23 +0300

    react@16.4

--- init.bat
+++ init.bat
@@ -1,5 +1,7 @@
 @pushd %~dp0\..
-@call npm install react react-dom babelify @babel/core @babel/preset-env @babel/preset-react @babel/plugin-proposal-class-properties classnames
+@call npm install react@16.4
+@call npm install react-dom@16.4 
+@call npm install babelify @babel/core @babel/preset-env @babel/preset-react @babel/plugin-proposal-class-properties classnames
 @call npm install envify uglify-js uglifyify 
 @call npm install react-autocomplete
 @call npm install react-select
