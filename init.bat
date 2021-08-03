2947a354d13715639dad41c69071430c2320794a
Shulkevich Vladislav <shulkevich@gmail.com>
2021-08-03 13:33:37 +0300

    Убрал указание react@16.4

--- init.bat
+++ init.bat
@@ -1,6 +1,6 @@
 @pushd %~dp0\..
-@call npm install react@16.4
-@call npm install react-dom@16.4 
+@call npm install react
+@call npm install react-dom
 @call npm install babelify @babel/core @babel/preset-env @babel/preset-react @babel/plugin-proposal-class-properties classnames
 @call npm install envify uglify-js uglifyify 
 @call npm install react-autocomplete
